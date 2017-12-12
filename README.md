734b677766fcedbeb70a7f8f63bdf04d5f03d7cf
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-12 09:33:08 -0800

    Add delete functionality.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 * add jquery-rails to Gemfile and application.js
 
+* add webpacker (not sure if this did anything)
+
 * use ```createReactClass``` instead of ```React.createClass```
 
 * use ```<input type="text" placeholder="Name" ref={(name) => { this.name = name; }} />``` instead of ```<input ref="name" />```
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -12,11 +12,29 @@ var Body = createReactClass({
     this.setState({ items: newState })
   },
 
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/items/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeItemClient(id);
+      }
+    });
+  },
+
+  removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+
+    this.setState({ items: newItems });
+  },
+
   render() {
     return (
       <div>
         <NewItem handleSubmit={this.handleSubmit} />
-        <AllItems items={this.state.items}/>
+        <AllItems items={this.state.items} handleDelete={this.handleDelete} />
       </div>
     )
   }
